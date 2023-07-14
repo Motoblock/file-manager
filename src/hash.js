a2428b80fbdd9c453f5afa381959f46233c25aec
Ludmila <milasasha@gmail.com>
2023-07-14 14:41:59 +0300

    refactor: change metod strem

--- src/hash.js
+++ src/hash.js
@@ -1,4 +1,4 @@
-import * as fs from 'node:fs/promises';
+import * as fs from 'node:fs';
 import { createHash } from 'node:crypto';
 import { getAbsolutePath, isExistFile } from './util.js';
 
@@ -6,12 +6,23 @@ export const hashFile = async (arrayFileName) => {
   const path = getAbsolutePath(arrayFileName);
   const isValid = await isExistFile(path);
 
-  if (isValid)
-    try {
-      const data = await fs.readFile(path, 'utf-8');
-      const hash = createHash('sha256').update(data).digest('hex');
-      console.log(hash);
-    } catch {
-      console.error("Operation failed");
-    }
+  if (isValid) {
+    const hash = createHash('sha256');
+    const input = fs.createReadStream(arrayFileName);
+    input.on('readable', () => {
+      const data = input.read();
+      if (data)
+        hash.update(data);
+      else {
+        console.log(`${hash.digest('hex')}`);
+      }
+    });
+  }
+  //   try {
+  //     const data = await fs.readFile(path, 'utf-8');
+  //     const hash = createHash('sha256').update(data).digest('hex');
+  //     console.log(hash);
+  //   } catch {
+  //     console.error("Operation failed");
+    // }
 };
